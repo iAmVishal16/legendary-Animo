6850efe1d074125bda42062deacc6b75320aff3e
Vishal Paliwal <paliwalvishal16@gmail.com>
2023-08-24 23:03:48 +0530

    Projects setup files

--- legendary-Animo/ContentView.swift
+++ legendary-Animo/ContentView.swift
@@ -8,17 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let rows:[RowView] = [
+        RowView(icon: "⭕️", title: "3D Ring animation", desc: "Circles animation in Z axis"),
+        RowView(icon: "⏳", title: "Countdown Timer for Fitness", desc: "A perfect fit in fitness app"),
+        RowView(icon: "🔺", title: "Triangle Animation", desc: "Multiple Gradient tringle shape scale animation"),
+        RowView(icon: "𑗊", title: "MultiShapes 3D animation with rotation", desc: "Multiple shapes rotation"),
+        RowView(icon: "᠅", title: "Dots Circle Animation", desc: "Dashed circles smooth animation using easeinout"),
+        RowView(icon: "🌇", title: "Google Photos Logo Animation", desc: "Google photos Logo animation using trim and offset"),
+        RowView(icon: "🔳", title: "Rectangle Rotation animation", desc: "Rectangle animation using scale, offset and easeinout curve"),
+        RowView(icon: "🪫", title: "Battery fill Waves with Bubbles animation", desc: "Rectangle animation using scale, offset and easeinout curve")
+
+        ]
+        
+        @State private var selection = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+        NavigationView {
+            ZStack (alignment: .top){
+                Color("BgColor")
+                    .edgesIgnoringSafeArea(.all)
+                
+                List {
+                    ForEach(rows.indices, id: \.self) { index in
+                        rows[index]
+                    }
+                }
+            }
+            .padding(.top, 8)
+            .listStyle(InsetGroupedListStyle())
+            .listRowSeparator(.hidden)
+            .listRowSpacing(8)
+            .navigationTitle("Legendary-Animo")
+            .navigationBarTitleDisplayMode(.automatic)
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
 }
--- legendary-Animo/Row.swift
+++ legendary-Animo/Row.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct RowView: View, Hashable {
+    var icon: String
+    var title: String
+    var desc: String
+
+    var body: some View {
+        ZStack(alignment: .center) {
+
+            HStack {
+                Text(icon)
+                    .font(.system(size: 36)).bold()
+                    .fontWeight(.semibold)
+                    .fontDesign(.rounded)
+                Spacer()
+            }
+
+            VStack (alignment: .leading, spacing: 8) {
+                HStack {
+                    Text(title)
+                        .font(.title2)
+                    Spacer()
+                }
+
+                Text(desc)
+                    .font(.callout)
+                    .fontWeight(.light)
+            }
+            .padding(.trailing, 16)
+            .padding(.leading, 54)
+            
+            HStack {
+               Spacer()
+                Image(systemName: "chevron.right")
+            }
+        }
+    }
+}
+
+struct RowView_Previews: PreviewProvider {
+    static var previews: some View {
+        RowView(icon: "😘", title: "Project name 1", desc: "This project create ")
+            .preferredColorScheme(.dark)
+    }
+}
